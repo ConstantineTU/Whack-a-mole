08227a7ae70a1823b29bee2144352cd99d42a239
ConstantineTU <8kozmo8@gmail.com>
2021-08-28 17:35:51 +0500

    feat: add js startGame - moleUp, moleDown

--- whack-a-mole/js/app.js
+++ whack-a-mole/js/app.js
@@ -1,5 +1,27 @@
+const moles = document.querySelectorAll('.game-mole')
+const holes = document.querySelectorAll('.game-hole')
+let score = 0
 
+function getScore(e) {
+	console.log(e)
+	let scoreBoard = document.querySelector('.score')
+	score++
+	scoreBoard.textContent = score
+}
 
+moles.forEach(mole => (mole.addEventListener('click', getScore)))
+moles.forEach(mole => (mole.addEventListener('click', function () {
+	for (hole of holes) {
+		hole.classList.remove('up')
+	}
+})))
 
+function randomMole() {
+	console.log(holes[Math.floor(Math.random() * holes.length)])
+	return holes[Math.floor(Math.random() * holes.length)].classList.add('up')
 
+}
 
+function startGame() {
+	randomMole()
+}
